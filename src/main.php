736360bcbb9203c997c1e329e1a72cd4e14efcb8
Gustavo Ribeiro <gugzribeiro@gmail.com>
2024-09-13 02:03:15 -0300

    Atualizações feitas no main do projeto

--- src/main.php
+++ src/main.php
@@ -7,13 +7,18 @@ require __DIR__ . "/../../vendor/autoload.php";
 // Usa a classe Login do namespace MeuProjeto\Login
 use MeuProjeto\model\Login;
 use MeuProjeto\model\UsuarioSistema;
+use MeuProjeto\model\Cadastrar;
 
 // Cria uma instância da classe Login
 $login = new Login();
+
 $sysu = new UsuarioSistema();
 $sysu->nome = "GustavoR";
+
+$cadastro = new Cadastrar();
 
 var_dump($sysu->nome);
 
 // Verifica o login (se o formulário foi enviado)
 $login->verificarLogin();
+$cadastro->getCadastrar();
