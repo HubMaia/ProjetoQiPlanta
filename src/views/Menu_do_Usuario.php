b41896e4c04e1f5a104b9ab2c3def9e7ef8ae942
O Famosoo Pedro <joaopedropaulohenrique@gmail.com>
2024-09-14 02:01:14 -0300

    commitado View

--- src/views/Menu_do_Usuario.php
+++ src/views/Menu_do_Usuario.php
@@ -7,6 +7,20 @@
     <link rel="icon" href="images/icons8-pinheiro-16.png" type="image/png">
 </head>
 <body>
-    <h1>Em Desenvovlvimento</h1>
+    <header>
+        <?php
+            include("header.php");
+        ?>
+    </header>
+    <main>
+        <center>
+            <div class="container">
+                <h1>Em Desenvovlvimento</h1>
+            </div>
+        </center>
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
--- src/views/header.php
+++ src/views/header.php
@@ -5,8 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial</title>
     <link rel="icon" href="images/icons8-pinheiro-16.png" type="image/png">
+    <link rel="stylesheet" href="./css/header.css">
 </head>
 <body>
+<header>
+    <!-- Logotipo -->
+    <img style="border-radius:50%" src="" alt="Logo">
+    <!-- Menu de navegação -->
+    <nav>
+      <!-- Lista de links -->
+      <ul>
+        <!-- Itens da lista -->
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Contato</a></li>
+      </ul>
+    </nav>
+  </header>
     <?php
        
         require_once 'header.php';
