12ee87db54ec5848383a5cd0b3a4aca2d6e0ea48
O Famosoo Pedro <joaopedropaulohenrique@gmail.com>
2024-09-16 12:16:46 -0300

    pequenas alterações nas classes dos inputs
    alterei algumas coisas com o css e as classe [css] , e só

--- src/index.php
+++ src/index.php
@@ -8,21 +8,20 @@
     <link rel="stylesheet" href="views/css/cadastro&login.css">
 <head>
     <div class="container">
-        <h1>Faça seu Login<br> ou<br> se Cadastre abaixo:</h1>
+            <h1>Login</h1>       
         <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-            <input type="text" name="usuario" placeholder="Digite o nome do Usuario" required>
-            <input type="password" name="senha" placeholder="Digite a Senha" required>
+            <input class="form-input" type="text" name="usuario" placeholder="Digite o nome do Usuario" required>
+            <input class="form-input" type="password" name="senha" placeholder="Digite a Senha" required>
            
             <div>
                 <button type="submit" class="login-btn">Login</button>
-                <button type="button" class="register-btn" onclick="window.location.href = './views/TelaCadastro.php';">Cadastrar</button>
+                <button type="button" class="register-btn" onclick="window.location.href = './views/TelaCadastro.php'">Cadastrar</button>
             </div>
         </form>
         <?php
 
 
 //Este código abaixo exibe os erros que o PHP der na tela do usuário propositalmente, para que assim possam fazer a verificação manual do problema que possa estar ocorrendo.
-
 //ini_set('display_errors', 1);
 //ini_set('display_startup_errors', 1);
 //error_reporting(E_ALL);
